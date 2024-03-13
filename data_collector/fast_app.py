fdb391aae5264ca58cce6cd90da759551dbc5b32
Carmine <carsomma@gmail.com>
2024-03-13 23:20:30 +0100

    #7 update get method fast api

--- data_collector/fast_app.py
+++ data_collector/fast_app.py
@@ -1,13 +1,13 @@
 from get_data import stream_data
 from fastapi import FastAPI 
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, JSONResponse
 from contextlib import asynccontextmanager
 import uvicorn
 import logging
 from pymongo import MongoClient
-from pymongo.errors import DuplicateKeyError
 import json
 import asyncio
+from bson import json_util
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
@@ -38,7 +38,7 @@ async def lifespan(task=background_insertion_task()):
 
 
 # Integrate lifespan event handler with FastAPI application
-fast_appl = FastAPI(lifespan=lifespan)
+fast_appl = FastAPI(lifespan=lifespan,default_response_class=JSONResponse)
 
 
 # GET endpoint to retrieve hello world message
@@ -46,22 +46,49 @@ fast_appl = FastAPI(lifespan=lifespan)
 async def root():
     return {"message": "Hello World"}
 
-# GET endpoint to fetch data from MongoDB
-@fast_appl.get("/fetch_data_from_mongo")
-async def fetch_data_one_by_one():
-    try:
-        cursor = collection.find()  # Get cursor for iterating over documents
-        return StreamingResponse(generate(cursor))
-    except Exception as e:
-        return {"error": f"An error occurred while fetching data: {e}"}
 
-async def generate(cursor):
-    for document in cursor:
-        # Convert ObjectId to string for JSON serialization
-        document["_id"] = str(document["_id"])
-        yield json.dumps(document, indent=4)
+# # GET endpoint to fetch data from MongoDB
+# @fast_appl.get("/fetch_data_from_mongo")
+# async def fetch_data_one_by_one():
+#     try:
+#         cursor = collection.find()  # Get cursor for iterating over documents
+#         return StreamingResponse(generate(cursor))
+#     except Exception as e:
+#         return {"error": f"An error occurred while fetching data: {e}"}
 
+# # Generator to convert cursor to JSON and yield line by line
+# async def generate(cursor):
+#     for document in cursor:
+#         # Convert ObjectId to string for JSON serialization
+#         document["_id"] = str(document["_id"])
+#         #yield json.dumps(document, indent=4) + ",\n"
+#         #yield JSONResponse(content=jsonable_encoder(document))
+#         #yield jsonable_encoder(document)
+#         yield json_util.dumps(document,indent=4) + ',\n'
 
+@fast_appl.get("/data")
+async def fetch_data():
+    async def _stream_data():
+        cursor = collection.find()  # Assuming you have a MongoDB collection
+        yield '['  # Start of the JSON array
+        for i, document in enumerate(cursor):
+            # Convert ObjectId to string for JSON serialization
+            document["_id"] = str(document["_id"])
+            if i == 0:
+                yield json.dumps(document,indent=4)
+            else:
+                yield ', ' + json.dumps(document,indent=4)
+        yield ']'  # End of the JSON array
+                
+    return StreamingResponse(_stream_data(), media_type='application/json')
+    # async def _stream_data():
+    #     cursor = collection.find()  # Assuming you have a MongoDB collection
+    #     for _, document in enumerate(cursor):
+    #         # Convert ObjectId to string for JSON serialization
+    #         document["_id"] = str(document["_id"])
+    #         yield json.dumps(document)
+                
+    # return StreamingResponse(_stream_data(), media_type='application/json')
 
 if __name__ == "__main__":
    uvicorn.run(fast_appl)
