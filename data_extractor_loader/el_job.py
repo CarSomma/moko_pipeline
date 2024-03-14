810f02954d0da8d3a8c3070c35a1599e418ca2a0
Carmine <carsomma@gmail.com>
2024-03-14 16:54:42 +0100

    #12 create el_job.py that implements a pipeline

---
+++ data_extractor_loader/el_job.py
@@ -0,0 +1,41 @@
+import dlt
+from dlt.sources.helpers import requests
+from dlt.pipeline.exceptions import PipelineStepFailed
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+
+@dlt.resource(write_disposition="append", columns={"location": {"data_type": "complex"}})
+def fastapi_resource():#api_secret_key=dlt.secrets.value):
+    url = "http://localhost:8000/fetch_data_from_mongo"
+    response = requests.get(url)#, params=params)
+    response.raise_for_status()
+    yield response.json()
+
+
+if __name__ == '__main__':
+    
+    
+    # configure the pipeline with your destination details
+    pipeline = dlt.pipeline(
+        pipeline_name='fastapi',
+        destination='duckdb',
+        dataset_name='fastapi_data',
+    )
+
+    # print the data yielded from resource
+    data = list(fastapi_resource())
+    print(data)
+
+    # run the pipeline with your parameters
+    try:
+        load_info = pipeline.run(fastapi_resource())
+        print(load_info)
+    except PipelineStepFailed as step_failed:
+        print(f"We failed at step: {step_failed.step} with step info {step_failed.step_info}")
+        raise
+    
+
+    
+    
