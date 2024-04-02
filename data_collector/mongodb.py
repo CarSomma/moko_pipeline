053bcfe92588210714e76f225283eb1ad90771d2
Carmine <carsomma@gmail.com>
2024-04-02 18:05:33 +0200

    update mongodb.py with new pipeline to aggregate coiunts

--- data_collector/mongodb.py
+++ data_collector/mongodb.py
@@ -5,31 +5,43 @@ import logging
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-HOST = "mongo_container" # "0.0.0.0" #
+HOST = "0.0.0.0" #"mongo_container" # "0.0.0.0" #
 PORT = 27017
 # Initialize MongoDB connection
 #client = MongoClient("mongodb://localhost:27017/")
 client = MongoClient(host=HOST,port=PORT)
-db = client["my_stream_database"]
-collection = db["my_stream_collection"]
+# db = client["my_stream_database"]
+# collection = db["my_stream_collection"]
+db = client["my_new_database"]
+collection = db["my_new_collection"]
 
-actions = ["view","click","purchase"]
+#actions = ["view","click","purchase"]
 #action_filter = {"location": {"$elemMatch": {"$regex": "^Europe"}}}
 
+# Define the aggregation pipeline
+pipeline = [
+    {"$unwind": "$action"},  # Deconstructs the array field "action" to process each action separately
+    {"$group": {"_id": "$action", "count": {"$sum": 1}}}  # Groups documents by action and counts occurrences
+]
 
-def count_actions(collection=collection, actions=actions):
-    counts_actions = {}
+def count_actions(collection=collection, pipeline=pipeline):
+    counts_actions = collection.aggregate(pipeline)
     counts = []
-    for action in actions:
-        action_filter = {"action": action}
-        count = collection.count_documents(action_filter)
+    actions = []
+    for document in counts_actions:
+        action = document["_id"]
+        count = document["count"]
         logging.info(f"Count for action {action}: {count}")
         counts.append(count)
-        counts_actions[action] = count
+        actions.append(action)
 
-    jcounts_actions = dict(actions=counts_actions)
-    return jcounts_actions, actions, counts
+    
+    return counts_actions, actions, counts
 
 if __name__ == "__main__":
-    jcounts_actions = count_actions(collection,actions)
-    print(jcounts_actions)
+    # Execute the aggregation pipeline
+    #result = collection.aggregate(pipeline)
+    counts_actions, actions, counts = count_actions(collection,pipeline)
+    # print(jcounts_actions)
+    print(actions)
+    print(counts)
